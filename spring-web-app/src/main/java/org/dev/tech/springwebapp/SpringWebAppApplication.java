3b1e6066fede8fef9d1e481cb2aee29620ae127f
devtechsolution <srivastvadevzone@gmail.com>
2019-06-07 12:38:35 +0530

    Merge

--- spring-web-app/src/main/java/org/dev/tech/springwebapp/SpringWebAppApplication.java
+++ spring-web-app/src/main/java/org/dev/tech/springwebapp/SpringWebAppApplication.java
@@ -2,8 +2,10 @@ package org.dev.tech.springwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="org.dev.tech")
 public class SpringWebAppApplication {
 
 	public static void main(String[] args) {
